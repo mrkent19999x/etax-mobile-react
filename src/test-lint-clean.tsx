f1593a841fd55fc2814607a38ce9eade23e84cda
mrkent19999x <mrkent19999x@github.com>
2025-09-16 10:37:46 +0700

    test: ca 2 - commit file src hợp lệ

---
+++ src/test-lint-clean.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// Test file hợp lệ để test commit PASS
+const TestLintClean: React.FC = () => {
+  return (
+    <div>
+      <h1>Test Lint Clean</h1>
+      <p>This file should pass lint check</p>
+    </div>
+  );
+};
+
+export default TestLintClean;
